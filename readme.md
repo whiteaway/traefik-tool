7a8d54ae623e423022ee7a43c97213eb5e1e5cbb
Morten D. Hansen <morten@visia.dk>
2018-02-19 16:01:35 +0100

    Added traefik bootstrapping and a .sh file for setup

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Træfik | your local docker server 
+
+## Do this
+Clone this repo to your machine. I have it in my homefolder in a subdirectory called 'tools'.
+
+
+Check the example-files for docker-compose.yml configuration file and traefik.toml file.  
+
+You should update these two files with a desired url you want to use locally to have traefik on.  
+Either put one in the files and copy them to your folder without example- in front, or run setup.sh (**only works on mac or linux**).
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "sets up your local hostname for træfik "
+
+echo "What should be the url of your environment? (example: traffik.dev.local)"
+read domain
+# cp example-files/example-docker-compose.yml docker-compose.yml
+# cp example-files/example-traefik.toml traefik.toml
+sed  "s/<insert-local-hostname>/$domain/g" example-files/example-docker-compose.yml > docker-compose.yml
+sed  "s/<insert-local-hostname>/$domain/g" example-files/example-traefik.toml > traefik.toml
+
+# find existing instances in the host file and save the line numbers
+matches_in_hosts="$(grep -n $domain /etc/hosts | cut -f1 -d:)"
+host_entry="127.0.0.1	$domain"
+
+echo "Please enter your password if requested."
+
+if [ ! -z "$matches_in_hosts" ]
+then
+    echo "Updating existing hosts entry."
+    # iterate over the line numbers on which matches were found
+    while read -r line_number; do
+        # replace the text of each line with the desired host entry
+        sudo sed -i '' "${line_number}s/.*/${host_entry}/" /etc/hosts
+    done <<< "$matches_in_hosts"
+else
+    echo "Adding new hosts entry."
+    echo "$host_entry" | sudo tee -a /etc/hosts > /dev/null
+fi
+
+echo "Now run docker-compose up and check http://$domain in your browser"
